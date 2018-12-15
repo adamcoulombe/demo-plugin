2a1740d2db24125adaca7929d300843781d4e4f4
Adam Coulombe <adamcoulombe@gmail.com>
2018-12-15 15:20:17 -0500

    Create demo-plugin.php

---
+++ demo-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Demo Plugin
+Plugin URI: http://www.adam.co/
+Description: Plugin to test
+Author: Adam Coulombe
+Author URI: http://www.adam.co/
+Version: 1.0
+Plugin Slug: demo-plugin
+
+*/
+
+?>
